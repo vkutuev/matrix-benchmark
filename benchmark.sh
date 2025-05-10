1dff8b9d4150b31b33b0bc4d51e8863abb0519e1
Vladimir Kutuev <vladimir.kutuev@gmail.com>
2025-05-10 23:02:46 +0300

    feat: add progress bar and multiple devices

--- benchmark.sh
+++ benchmark.sh
@@ -17,11 +17,10 @@ if [[ "$INSTALL_DEPS" != 0 ]]; then
 	echo "----------"
 	sudo apt update
 
-	# echo "Install OpenBLAS deps"
-	# echo "---------------------"
-
-	echo "Install dotnet"
+	echo "Install deps"
 	echo "--------------"
+	sudo apt install python3-dev build-essential \
+		cmake git pkg-config make ninja-build
 	sudo apt install -y dotnet-sdk-9.0
 fi
 
@@ -43,11 +42,11 @@ if [[ "$BUILD_POCL" != 0 ]]; then
 	ARCH=$(uname -m)
 	POCL_CMAKE_FLAGS=""
 	case "$ARCH" in
-		"x86_64") POCL_CMAKE_FLAGS="-DKERNELLIB_HOST_CPU_VARIANTS=distro"
+		"x86_64") POCL_CMAKE_FLAGS=("-DKERNELLIB_HOST_CPU_VARIANTS=distro")
 			;;
-		"riscv64") POCL_CMAKE_FLAGS="-DLLC_HOST_CPU=spacemit-x60 -DHOST_CPU_TARGET_ABI=lp64d"
+		"riscv64") POCL_CMAKE_FLAGS=("-DLLC_HOST_CPU=spacemit-x60" "-DHOST_CPU_TARGET_ABI=lp64d")
 			;;
-		"aarch64") POCL_CMAKE_FLAGS="-DLLC_HOST_CPU=cortex-a55"
+		"aarch64") POCL_CMAKE_FLAGS=("-DLLC_HOST_CPU=cortex-a55")
 			;;
 		*) echo "Unknown arch" && exit 1
 			;;
@@ -63,7 +62,7 @@ if [[ "$BUILD_POCL" != 0 ]]; then
 		-DCMAKE_INSTALL_PREFIX=/opt/pocl \
 		-DCMAKE_INSTALL_LIBDIR=lib \
 		-DINSTALL_OPENCL_HEADERS=OFF \
-		"$POCL_CMAKE_FLAGS" \
+		"${POCL_CMAKE_FLAGS[@]}" \
 		&& cmake --build "$B" \
 		&& sudo cmake --build "$B" --target install
 fi
@@ -72,18 +71,52 @@ if [[ "$ENABLE_OPENBLAS" != 0 ]]; then
 	S="$R/OpenBLAS"
 	B="$S/build"
 	if [[ "$BUILD_OPENBLAS" != 0 ]]; then
+		echo "Build OpenBLAS"
+		echo "--------------"
 		cmake -S "$S" -B "$B" \
 			-DCMAKE_BUILD_TYPE=Release -G Ninja \
 			-DBUILD_BENCHMARKS=ON -DBUILD_SHARED_LIBS=ON \
 			&& cmake --build "$B"
 	fi
 
-	for N in "${MATRICES_SIZES[@]}"; do
+	echo "Run OpenBLAS benchmark"
+	echo "----------------------"
+	echo "matrix,time" > "${R}/${OUT_OPENBLAS}"
+	for i in ${!MATRICES_SIZES[*]}; do
+		N=${MATRICES_SIZES[$i]}
+		printf '\rProcess matrix: %d  [%d/%d]' "$N" "$(( i + 1 ))" "${#MATRICES_SIZES[*]}"
 		OPENBLAS_LOOPS="$RUNS_NUMBER" "$B/benchmark_gemm" "$N" "$N" 2>&1 \
 			| sed '1,2d' \
-			| awk -v RUNS="$RUNS_NUMBER" '{ print 1000 * $7 / RUNS }' >> "$R/$OUT_OPENBLAS"
+			| awk -v MATRIX="$N" -v RUNS="$RUNS_NUMBER" \
+				'{ print MATRIX, 1000 * $7 / RUNS }' >> "${R}/${OUT_OPENBLAS}"
 	done
+	echo ""
 fi
+
+run_clblast() {
+	PLATFORM="$1"
+	S="$R/CLBlast"
+	B="$S/build"
+	echo "Run CLBlast benckmark for ${PLATFORM} platform"
+	echo "------------------------------------"
+	clinfo -d "${PLATFORM}:${OPENCL_DEVICE}" -l
+	OUT_CLBLAST="${R}/clblast_${PLATFORM}_${OPENCL_DEVICE}.csv"
+	echo "matrix,time" > "$OUT_CLBLAST"
+	for i in ${!MATRICES_SIZES[*]}; do
+		N=${MATRICES_SIZES[$i]}
+		printf '\rProcess matrix: %d  [%d/%d]' "$N" "$(( i + 1 ))" "${#MATRICES_SIZES[*]}"
+		"$B/clblast_client_xgemm" \
+			-platform "$PLATFORM" -device "$OPENCL_DEVICE" \
+			-precision 32 -runs "$RUNS_NUMBER" -warm_up -q -no_abbrv \
+			-clblas 0 -cblas 0 -cublas 0 \
+			-m "$N" -n "$N" -k "$N" \
+			-layout 101 -transA 111 -transB 111 \
+			-step 0 -num_steps 1 \
+			| cut -f 15 -d ';' | sed '1d' | tr -d '[:space:]' \
+			| awk -v MATRIX="$N" 'BEGIN{ OFS="," } { print MATRIX, $1 }' >> "$OUT_CLBLAST"
+	done
+	echo ""
+}
 
 if [[ "$ENABLE_CLBLAST" != 0 ]]; then
 	S="$R/CLBlast"
@@ -96,21 +129,54 @@ if [[ "$ENABLE_CLBLAST" != 0 ]]; then
 			-DCLIENTS=ON \
 			&& cmake --build "$B"
 	fi
-
-	echo "Run CLBlast benckmark"
-	echo "---------------------"
-	for N in "${MATRICES_SIZES[@]}"; do
-		"$B/clblast_client_xgemm" \
-			-platform "$OPENCL_PLATFORM" -device "$OPENCL_DEVICE" \
-			-precision 32 -runs 10 -warm_up -q -no_abbrv \
-			-clblas 0 -cblas 0 -cublas 0 \
-			-m "$N" -n "$N" -k "$N" \
-			-layout 101 -transA 111 -transB 111 \
-			-step 0 -num_steps 1 \
-			| cut -f 15 -d ';' | sed '1d' | tr -d '[:space:]' >> "$R/$OUT_CLBLAST"
-			echo "" >> "$R/$OUT_CLBLAST"
+	for platform in "${OPENCL_PLATFORM[@]}"; do
+		run_clblast "$platform"
 	done
 fi
+
+run_brahma() {
+	KERNEL="$1"
+	DEV="$2"
+	pushd "$R/ImageProcessing" || exit 1
+	echo "Run Brahma benckmark with kernel ${KERNEL} and device ${DEV}"
+	echo "-----------------------------------------------------"
+	if [[ "$BRAHMA_AUTOTUNE" != 0 ]]; then
+		echo "Run tune.py for ${KERNEL} and device ${DEV}"
+		echo "------------------------------------"
+		sed -i "s/kernels = \[.*\]/kernels = [\'${KERNEL}\']/g" ./tune.py
+		sed -i "s/platform = '.*'/platform = '${DEV}'/g" ./tune.py
+		sed -i "s/types = \[.*\]/types = [\'${BRAHMA_TYPE}\']/g" ./tune.py
+		python3 ./tune.py
+		WGS=$(head -n 1 "./tuning_results/${KERNEL}_${DEV}_mt-float32_1024_arithmetic.log" \
+			| cut -f 1 -d ',')
+		WPT=$(head -n 1 "./tuning_results/${KERNEL}_${DEV}_mt-float32_1024_arithmetic.log" \
+			| cut -f 2 -d ',' | tr -d '[:space:]')
+	else
+		WGS="$BRAHMA_WGS_DEFAULT"
+		WPT="$BRAHMA_WPT_DEFAULT"
+	fi
+	OUT_BRAHMA="${R}/brahma_${KERNEL}_${DEV}_${WGS}_${WPT}.csv"
+	echo "matrix,time" > "${R}/${OUT_BRAHMA}"
+	for i in ${!MATRICES_SIZES[*]}; do
+		N=${MATRICES_SIZES[$i]}
+		printf '\rProcess matrix: %d  [%d/%d]' "$N" "$(( i + 1 ))" "${#MATRICES_SIZES[*]}"
+		dotnet ./src/MatrixMultiplication/bin/Release/net9.0/MatrixMultiplication.dll \
+			--platform "$DEV" \
+			--workgroupsize "$WGS" \
+			--workperthread "$WPT" \
+			--matrixsize "$N" \
+			--kernel "$KERNEL" \
+			--matrixtype mt-float32 \
+			--semiring arithmetic \
+			--numtorun "$RUNS_NUMBER" \
+			| sed '1d' \
+			| cut -f 3 -d ' ' \
+			| awk -v MATRIX="$N" -v RUNS="$RUNS_NUMBER" \
+			'	BEGIN{ OFS="," } { print MATRIX, $1 / RUNS }' >> "$OUT_BRAHMA"
+	done
+	echo ""
+	popd || exit 1
+}
 
 if [[ "$ENABLE_BRAHMA" != 0 ]]; then
 	pushd "$R/ImageProcessing" || exit 1
@@ -119,23 +185,12 @@ if [[ "$ENABLE_BRAHMA" != 0 ]]; then
 		echo "-----------------------------"
 		dotnet build -c Release
 	fi
-	echo "Run Brahma benckmark"
-	echo "---------------------"
-	for N in "${MATRICES_SIZES[@]}"; do
-		dotnet ./src/MatrixMultiplication/bin/Release/net9.0/MatrixMultiplication.dll \
-			--platform "$BRAHMA_DEV" \
-			--workgroupsize "$BRAHMA_WGS" \
-			--workperthread "$BRAHMA_WPT" \
-			--matrixsize "$N" \
-			--kernel "$BRAHMA_KERNEL" \
-			--matrixtype mt-float32 \
-			--semiring arithmetic \
-			--numtorun "$RUNS_NUMBER" \
-			| sed '1d' \
-			| cut -f 3 -d ' ' \
-			| awk -v RUNS="$RUNS_NUMBER" '{ print $1 / RUNS }' >> "$R/$OUT_BRAHMA"
-	done
 	popd || exit 1
+	for kernel in "${BRAHMA_KERNEL[@]}"; do
+		for dev in "${BRAHMA_DEV[@]}"; do
+			run_brahma "$kernel" "$dev"
+		done
+	done
 fi
 
 popd || exit 1
--- conf.sh
+++ conf.sh
@@ -9,22 +9,18 @@ BUILD_BRAHMA=1
 
 INSTALL_DEPS=0
 
-MATRICES_SIZES=(512 1024 2048 4096 8192)
-RUNS_NUMBER=2
+MATRICES_SIZES=(512 1024 1536 2048 2560 3072 3584 4096 4608 5120 5632 6144 6656 7168 7680 8192)
+RUNS_NUMBER=10
 
 # POCL configuration
 # ------------------
-BUILD_POCL=1
+BUILD_POCL=0
 LLVM_VERSION=18
-
-# OpenCL configuration
-# --------------------
-OPENCL_PLATFORM=0
-OPENCL_DEVICE=0
 
 # CLBlast configuration
 # ---------------------
-OUT_CLBLAST=clblast_${OPENCL_PLATFORM}_${OPENCL_DEVICE}.csv
+OPENCL_PLATFORM=(0 1)
+OPENCL_DEVICE=0
 
 # OpenBLAS configuration
 # ----------------------
@@ -32,9 +28,10 @@ OUT_OPENBLAS=openblas.csv
 
 # ImageProcessing configuration
 # -----------------------------
-BRAHMA_WGS=8
-BRAHMA_WPT=4
-BRAHMA_DEV=anygpu
-BRAHMA_KERNEL=k4
-OUT_BRAHMA="brahma_${BRAHMA_KERNEL}_${BRAHMA_DEV}.csv"
+BRAHMA_AUTOTUNE=1
+BRAHMA_WGS_DEFAULT=8
+BRAHMA_WPT_DEFAULT=4
+BRAHMA_DEV=("anygpu" "poclcpu")
+BRAHMA_KERNEL=("k3" "k4")
+BRAHMA_TYPE="mt-float32"
 export BRAHMA_OCL_PATH=/usr/lib/libOpenCL.so.1.0.0
